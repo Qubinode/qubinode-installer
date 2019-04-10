d58e91811b3e8b2b0202e9e79a4e81f2701e0f98
Tosin Akinosho <takinosh@redhat.com>
2019-04-10 13:06:34 -0400

    started cleanup for code

--- start_deployment.sh
+++ start_deployment.sh
@@ -11,6 +11,10 @@
 #/ EXAMPLES
 #/
 #########################
+# import functions      #
+#########################
+source dns_server/lib/dns_functions.sh
+#########################
 # The command line help #
 #########################
 function display_help() {
@@ -94,118 +98,8 @@ function addgluster() {
   fi
 }
 
-function configure_dns_for_arecord() {
-    DNSSERVER=$(cat ${1} | grep dns_servers=* | tr -d '"[]",' | awk '{print $1}' | cut -d'=' -f2)
-    DOMAINNAME=$(cat ${1} | grep search_domain| awk '{print $1}' | cut -d'=' -f2)
-    lastip=`echo "$DNSSERVER" | sed 's/^.*\.\([^.]*\)$/\1/'`
-    echo "Generating dns_server/update_dns_server_${lastip}_entry.yml" || exit 1
-
-    cp  dns_server/update_dns_server_entry.yml  dns_server/update_dns_server_${lastip}_entry.yml  || exit 1
-    sed -ri 's/^(\s*)(zone\s*:\s*"example.com."\s*$)/\      zone: "'$DOMAINNAME'."/' dns_server/update_dns_server_${lastip}_entry.yml || exit 1
-    sed -ri 's/^(\s*)(server\s*:\s*"0.0.0.0"\s*$)/\      server: "'$DNSSERVER'"/' dns_server/update_dns_server_${lastip}_entry.yml || exit 1
-
-    echo -e "\e[32m************************\e[0m"
-    read -p "\e[32m*-0 : \e[0m" DNS_KEY_NAME
-    echo -e "\e[32m************************\e[0m"
-    sed -ri 's/^(\s*)(key_name\s*:\s*"example.key"\s*$)/\      key_name: "'$DNS_KEY_NAME'."/' dns_server/update_dns_server_${lastip}_entry.yml || exit 1
-
-    echo -e "\e[32m************************\e[0m"
-    echo "\e[32m*Key Secert for DNS server this will allow script to write a records to your dnsserver: \e[0m"
-    echo -e "\e[32m************************\e[0m"
-    unset SECERT_KEY;
-    while IFS= read -r -s -n1 key; do
-      if [[ -z $key ]]; then
-         echo
-         break
-      else
-         echo -n '*'
-         SECERT_KEY+=$key
-      fi
-    done
-#echo "${SECERT_KEY}" > dns_server/dns_key/dns_key
-cat <<YAML > dns_server/dns_key/dns_key
----
-vault_dns_key: ${SECERT_KEY}
-
-YAML
-
-  echo -e "\e[32m************************\e[0m"
-  echo "\e[32m*Enter Ansible Vault password: \e[0m"
-  echo -e "\e[32m************************\e[0m"
-  unset SECERT_KEY;
-  while IFS= read -r -s -n1 key; do
-    if [[ -z $key ]]; then
-       echo
-       break
-    else
-       echo -n '*'
-       SECERT_KEY+=$key
-    fi
-  done
-    echo "${SECERT_KEY}" >   ansible-vault.pass
-
-    ansible-vault encrypt dns_server/dns_key/dns_key   --vault-password-file=ansible-vault.pass
-
-    echo "Verifing DNS Configuration"
-    cat dns_server/update_dns_server_${lastip}_entry.yml | grep "${DNSSERVER}"  || exit 1
-    cat dns_server/update_dns_server_${lastip}_entry.yml
-    read -n 1 -s -r -p "Press any key to continue"
-}
-
-function set_arecord() {
-    DOMAINNAME=$(cat ${1}| grep search_domain| awk '{print $1}' | cut -d'=' -f2)
-    DOMAINPREFIX=$(echo $DOMAINNAME | cut -d'.' -f1)
-
-    DNSSERVER=$(cat ${1} | grep dns_servers=* | tr -d '"[]",' | awk '{print $1}' | cut -d'=' -f2)
-    lastip=`echo "$DNSSERVER" | sed 's/^.*\.\([^.]*\)$/\1/'`
-
-    JUMPBOXIP=$(cat jumpbox | tr -d '"[]",')
-    ansible-playbook -i inventory.vm.dnsserver dns_server/update_dns_server_${lastip}_entry.yml  --extra-vars="a_record=jumpbox.${DOMAINPREFIX}" --extra-vars="ip_address=${JUMPBOXIP}" --extra-vars="rhel_user=${2}" --vault-password-file=ansible-vault.pass || exit 1
-    MASTERIP=$(cat master | tr -d '"[]",')
-    ansible-playbook -i inventory.vm.dnsserver dns_server/update_dns_server_${lastip}_entry.yml  --extra-vars="a_record=master.${DOMAINPREFIX}" --extra-vars="ip_address=${MASTERIP}" --extra-vars="rhel_user=${2}" --vault-password-file=ansible-vault.pass || exit 1
-
-    NODES=$(ls node*)
-
-    for n in $NODES; do
-      NODEIP=$(cat $n | tr -d '"[]",')
-      ansible-playbook -i inventory.vm.dnsserver dns_server/update_dns_server_${lastip}_entry.yml  --extra-vars="a_record=${n}.${DOMAINPREFIX}" --extra-vars="ip_address=${NODEIP}" --extra-vars="rhel_user=${2}" --vault-password-file=ansible-vault.pass || exit 1
-      INFRANODE=$(cat inventory.3.11.centos.gluster | grep node-config-infra | tr   = " " | awk '{print $1}')
-      if [[ $n ==  $INFRANODE ]]; then
-        APPENDPOINT=$(cat inventory.3.11.centos.gluster | grep openshift_master_default_subdomain= | tr   = " " | awk '{print $2}')
-        ansible-playbook -i inventory.vm.dnsserver dns_server/update_dns_server_${lastip}_entry.yml  --extra-vars="a_record=*.${APPENDPOINT}" --extra-vars="ip_address=${NODEIP}" --extra-vars="rhel_user=${2}" --vault-password-file=ansible-vault.pass || exit 1
-      fi
-    done
-
-
-  rm  ansible-vault.pass
-
-}
-
-function configurednsforopenshift() {
-  read -p "Do you need a dns server secret key  created? " -n 1 -r
-  echo    # (optional) move to a new line
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
-    if [[ ! -f inventory.vm.dnsserver ]]; then
-      read -p "Enter DNS IP address: " DNS_IP
-cat <<EOF > inventory.vm.dnsserver
-[dns_server]
-${DNS_IP}
-
-EOF
-    fi
-
-    DNS_ZONE=$(cat ${1} | grep search_domain| awk '{print $1}' | cut -d'=' -f2)
-    DOMAINNAME=$(echo $DNS_ZONE | cut -d'.' -f2)
-    echo -e "\e[32m************************\e[0m"
-    read -p "\e[32mEnter Key Name that Ansible will use to write to dns server: \e[0m" DNS_KEY
-    echo -e "\e[32m************************\e[0m"
-    ansible-playbook -i inventory.vm.dnsserver dns_server/configure_dns_server_for_openshift.yml  --extra-vars="zone_name=${DNS_ZONE}" --extra-vars="key_name=${DNS_KEY}" --extra-vars="dns_server_ip=${DNS_IP}"  --extra-vars="rhel_user=$2" --extra-vars="domain_name=${DOMAINNAME}.com"
-  fi
-}
-
 main() {
-    # check_args "${@}"
+    # check_args "${@}"dns_servers
     :
     if [ "$1" == "-h" ] ; then
       display_help
@@ -217,7 +111,7 @@ main() {
       addssh
       configurednsforopenshift $2 centos
 
-      configure_dns_for_arecord $2
+      configure_dns_for_arecord $2 centos
       echo -e "\e[32m************************\e[0m"
       echo -e "\e[32mDeploying Openshift vms\e[0m"
       echo -e "\e[32m************************\e[0m"
@@ -249,13 +143,14 @@ main() {
 
       scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $3  centos@${JUMPBOX}:~/openshift-ansible
 
-      set_arecord $2 centos
+      set_arecord $2 centos $3
+
     elif [[ "$1" == "rhel" ]]  && [[ -f "$2"  ]] && [[ -f "$3"  ]]; then
       read -p "Enter USERNAME for vm login: " RHEL_USER
       validation $2
       addssh
       configurednsforopenshift $2 ${RHEL_USER}
-      configure_dns_for_arecord $2
+      configure_dns_for_arecord $2 ${RHEL_USER}
       echo -e "\e[32mDeploying Openshift vms\e[0m"
       $USESUDO  ansible-playbook -i $2 deploy_openshift_vms.yml  --become  || exit 1
       echo -e "\e[32mCreating inventory files from newly created vms\e[0m"
@@ -283,7 +178,8 @@ main() {
 
       scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $3  ${RHEL_USER}@${JUMPBOX}:~/openshift-ansible
 
-      set_arecord $2 ${RHEL_USER}
+      set_arecord $2 ${RHEL_USER} $3
+
     else
       display_help
     fi
