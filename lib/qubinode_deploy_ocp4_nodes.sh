e81a5e224cb873e1077c1536c792a67e455da602
Tosin Akinosho <tosin.akinosho@gmail.com>
2019-11-05 22:40:32 -0500

    adding additional playbooks for deployment

---
+++ lib/qubinode_deploy_ocp4_nodes.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+BOOTSTRAP=$(sudo virsh net-dumpxml ocp42 | grep  bootstrap | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
+ansible-playbook playbooks/ocp4_07_deploy_bootstrap_vm.yml  -e vm_mac_address=${BOOTSTRAP}
+
+for i in {0..2}
+do
+    MASTER=$(sudo virsh net-dumpxml ocp42 | grep  master-${i} | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
+    ansible-playbook playbooks/ocp4_07.1_deploy_master_vm.yml  -e vm_mac_address=${MASTER}   -e vm_name=master-${i}
+    sleep 10s
+done
+
+for i in {0..1}
+do
+  COMPUTE=$(sudo virsh net-dumpxml ocp42 | grep  compute-${i} | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
+  ansible-playbook playbooks/ocp4_07.1_deploy_master_vm.yml  -e vm_mac_address=${COMPUTE}   -e vm_name=compute-${i}
+  sleep 10s
+done
+
+
+i="$(sudo virsh list | grep running |wc -l)"
+
+while [ $i -gt 1 ]
+do
+  echo "waiting for coreos first boot to complete current count ${i}"
+  sleep 10s
+  i="$(sudo virsh list | grep running |wc -l)"
+done
