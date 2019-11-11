c33ef6f1a2b119b6e70919236b069de05deff7da
Rodrique Heron <rheron@rodhouse.org>
2019-11-10 20:06:13 -0500

    saving changes before rebuild

--- lib/openshift-smoke-test.sh
+++ lib/openshift-smoke-test.sh
@@ -1,42 +1,14 @@
 #!/bin/bash
 
-function config_err_msg () {
-    cat << EOH >&2
-  There was an error finding the full path to the qubinode-installer project directory.
-EOH
-}
+web_console="https://qbn-ocp3-master01.lunchnet.example:8443"
+ocp_user=admin33
 
-# this function just make sure the script
-# knows the full path to the project directory
-# and runs the config_err_msg if it can't determine
-# that start_deployment.conf can find the project directory
-function setup_required_paths () {
-    current_dir="`dirname \"$0\"`"
-    project_dir="$(dirname ${current_dir})"
-    project_dir="`( cd \"$project_dir\" && pwd )`"
-    if [ -z "$project_dir" ] ; then
-        config_err_msg; exit 1
-    fi
-
-    if [ ! -d "${project_dir}/playbooks/vars" ] ; then
-        config_err_msg; exit 1
-    fi
-}
-
-setup_required_paths
-openshift3_variables
-
-function sleep_for_a_sec() {
-  sleep 5s
-}
-
-echo "******************************************"
-echo "***   Qubinode OpenShift Smoke Test    ***"
-echo "******************************************"
-sleep_for_a_sec
-
-
-oc login ${web_console}:8443  -u ${ocp_user}
+oc login ${web_console} --username=${ocp_user} --password=redhat2019 --insecure-skip-tls-verify=true
+if [ $? -eq 1 ]
+then
+    echo "Log to $web_console failed as user $ocp_user"
+    exit 1
+fi
 oc new-project validate
 oc new-app nodejs-mongo-persistent
 
@@ -74,3 +46,4 @@ echo "*** SMOKE TESTS COMPLTED SUCCESSFULLY  ***"
 echo "******************************************"
 
 exit 0
+
