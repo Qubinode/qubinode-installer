d38a1351a06a4365688d7bd9ec274b17c99b29ca
Rodrique Heron <rheron@rodhouse.net>
2020-01-04 12:43:53 -0500

    fixes #102

--- lib/qubinode_hardware_profile.sh
+++ lib/qubinode_hardware_profile.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 function collect_system_information() {
-  MANUFACTURER=$(sudo dmidecode --string system-manufacturer)
-  PRODUCTNAME=$(sudo dmidecode --string baseboard-product-name)
-  AVAILABLE_MEMORY=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
-  AVAILABLE_HUMAN_MEMORY=$(free -h | awk '/Mem/ {print $2}')
+    MANUFACTURER=$(sudo dmidecode --string system-manufacturer)
+    PRODUCTNAME=$(sudo dmidecode --string baseboard-product-name)
+    AVAILABLE_MEMORY=$(awk '/MemTotal/ {print $2}' /proc/meminfo)
+    AVAILABLE_HUMAN_MEMORY=$(free -h | awk '/Mem/ {print $2}')
 
 
-  libvirt_pool_name=$(cat playbooks/vars/all.yml | grep libvirt_pool_name: | awk '{print $2}')
-  AVAILABLE_STORAGE=$(sudo virsh pool-list --details | grep "${libvirt_pool_name}" |awk '{print $5*1024}')
-  AVAILABLE_HUMAN_STORAGE=$(sudo virsh pool-list --details | grep "${libvirt_pool_name}" |awk '{print $5,$6}')
+    libvirt_pool_name=$(cat playbooks/vars/all.yml | grep libvirt_pool_name: | awk '{print $2}')
+    AVAILABLE_STORAGE=$(sudo virsh pool-list --details | grep "${libvirt_pool_name}" |awk '{print $5*1024}')
+     AVAILABLE_HUMAN_STORAGE=$(sudo virsh pool-list --details | grep "${libvirt_pool_name}" |awk '{print $5,$6}')
 }
 
 if [[ ! -f qubinode_profile.log ]]; then
-  rm -rf qubinode_profile.log
-  collect_system_information
+    rm -rf qubinode_profile.log
+    collect_system_information
 cat >qubinode_profile.log<<EOF
 Manufacturer: ${MANUFACTURER}
 Product Name: ${PRODUCTNAME}
@@ -34,6 +34,7 @@ CPU INFO
 $(lscpu | egrep 'Model name|Socket|Thread|NUMA|CPU\(s\)')
 EOF
 
+fi
+
 echo "SYSTEM REPORT"
 cat qubinode_profile.log
-fi
