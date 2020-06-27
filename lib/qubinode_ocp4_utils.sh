288628dfd20e2be89c9653c0311b2fad05f4dfd6
Rodrique Heron <rheron@rodhouse.org>
2020-06-26 20:15:10 -0400

    refactor the startup and shutdown of the cluster from bash to ansible Fixed issue #239 Fixed issue #228 replaced master/masters with controlplane in all documentation replaced controlplane node names with ctrlplane in all documentation Working towards issue #241

--- lib/qubinode_ocp4_utils.sh
+++ lib/qubinode_ocp4_utils.sh
@@ -1086,111 +1086,6 @@ function configure_nfs_storage () {
     fi
 }
 
-function shutdown_variables () {
-    export KUBECONFIG=/home/admin/qubinode-installer/ocp4/auth/kubeconfig
-    REGISTER_STATUS=$(oc get clusteroperators | awk '/image-registry/ {print $3}')
-    CLUSTER_UPTIME=$(oc get clusteroperators | awk '/authentication/ {print $6}')
-    CLUSTER_UUID=$(oc get clusterversions.config.openshift.io version -o jsonpath='{.spec.clusterID}{"\n"}')
-    INFRA_ID=$(oc get infrastructures.config.openshift.io cluster -o jsonpath='{.status.infrastructureName}{"\n"}')
-    BKUP_CMD="sudo /usr/local/bin/etcd-snapshot-backup.sh ./assets/backup/snapshot.db"
-    NODE_USER="core"
-    SSH_USER=$(whoami)
-    USER_SSH_ID="/home/${SSH_USER}/.ssh/id_rsa"
-    SSH_OPTIONS="-q -o StrictHostKeyChecking=no -o BatchMode=yes"
-    HOURS_RUNNING=$(oc get clusteroperators | awk '/authentication/ {print $6}'|tr -d 'h'|tr -d 'd')
-}
-
-function shutdown_nodes () {
-    # https://access.redhat.com/solutions/4037631
-    # https://access.redhat.com/solutions/4271712
-    # https://access.redhat.com/solutions/4218311
-    shutdown_variables
-    MASTER_ONE=192.168.50.10
-    MASTER_STATE=$(ping -c3 ${MASTER_ONE} 1>/dev/null; echo $?)
-    
-    if [ $MASTER_STATE -ne 0 ]
-    then
-        printf "\n It appears the cluster is already down.\n\n"
-        exit 0
-    else
-       printf "\ Shutting down the ocp4 cluster.\n"
-    fi
-    
-
-    for node in $(echo $NODES)
-    do
-        VM_NAME=$(echo $node|cut -d\. -f1)
-        VM_STATE=$(sudo virsh dominfo --domain $VM_NAME | awk '/State/ {print $2}')
-        if [ $VM_STATE == "running" ]
-        then
-
-            if [ "A${ROLE}" == "Acompute" ]
-            then
-                # mark node unschedulable
-                oc adm cordon $node
-                if [ "A$?" != "A0" ]
-                then
-                    printf "\n Marking $node unschedulable returned $?.\n"
-                    printf "\n Please investigate and try again.\n"
-                    exit 1
-                fi
-
-                # drain node
-                oc adm drain $node --ignore-daemonsets --delete-local-data --force --timeout=120s
-                if [ "A$?" != "A0" ]
-                then
-                    printf "\n Draining $node returned $?.\n"
-                    printf "\n Continining with shtudwon. Please investigate and try again.\n"
-                    # This should prompt the user and ask if they would like t continue with
-                    # shutdown or exist and troubleshoot.
-                    #exit 1
-                fi
-            fi 
-
-            printf "\n\n Shutting down $node.\n"
-            ssh $SSH_OPTIONS -i $USER_SSH_ID "${NODE_USER}@${node}" sudo shutdown -h now --no-wall
-
-            until [ $VM_STATE != "running" ]
-            do
-                printf "\n Waiting on $VM_NAME to shutdown. \n"
-                VM_STATE=$(sudo virsh dominfo --domain $VM_NAME | awk '/State/ {print $2}')
-                sleep 5s
-            done
-            printf "\n $VM_NAME state is $VM_STATE\n\n"
-        else 
-            printf "\n $VM_NAME state is $VM_STATE\n\n"
-        fi
-    done
-}
-
-function shutdown_cluster () {
-    shutdown_variables
-    if [ $HOURS_RUNNING -gt 24 ]
-    then
-    
-        printf "\n The ocp4 cluster has been up for more than 24hrs now.\n The current uptime is ${HOURS_RUNNING}\n\n"
-        ALL_COMPUTES=$(oc get nodes -l node-role.kubernetes.io/worker="" --no-headers | awk '{print $1}'|sort -r)
-        ALL_MASTERS=$(oc get nodes -l node-role.kubernetes.io/master="" --no-headers | awk '{print $1}'|sort -r)
-    
-        printf "\n Backing up etcd snapshot.\n\n"
-        ssh $SSH_OPTIONS -i $USER_SSH_ID "${NODE_USER}@${MASTER_ONE}" $BKUP_CMD tar tar czf - /home/core/assets/ > /home/${SSH_USER}/ocp4-etcd-snapshot-$(date +%Y%m%d-%H%M%S).tar.gz
-    
-        # Mark computes as unscheduleable, drain and shutdown
-        ROLE=compute
-        NODES=$ALL_COMPUTES
-        shutdown_nodes 
-    
-        ROLE=master
-        NODES=$ALL_MASTERS
-        shutdown_nodes 
-    else
-        printf "\n The ocp4 cluster has been up for less that 24hrs now.\n Please wait until after 24rs beforetrying to shutdown the cluster.\n\n"
-    fi
-    
-    exit 0
-
-}
-
 function remove_ocp4_worker () {
     # Get user provides options
     for var in "${product_options[@]}"
@@ -1364,10 +1259,10 @@ openshift4_server_maintenance () {
            echo  "Running smoke test on environment: : not implemented yet"
               ;;
        shutdown)
-            shutdown_cluster
+            ansible-playbook ${project_dir}/playbooks/deploy_ocp4.yml -t shutdown -e shutdown_cluster=yes || exit 1
             ;;
        startup)
-            ansible-playbook ${project_dir}/playbooks/deploy_ocp4.yml -t startup -e startup_cluster=yes || exit 1
+            ansible-playbook ${project_dir}/playbooks/deploy_ocp4.yml -t startup -e startup_cluster=yes -vvvvv|| exit 1
             /usr/local/bin/qubinode-ocp4-status
             ;;
        status)
