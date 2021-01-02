b04298a1300a245877d8c61d300db82e17272fd2
Rodrique Heron <rheron@rodhouse.org>
2021-01-02 09:11:34 -0500

    add option to exit from firstboot while loop if detected

--- lib/qubinode_menu_welcome.sh
+++ lib/qubinode_menu_welcome.sh
@@ -65,6 +65,11 @@ display_openshift_msg_ocp4 () {
         qubinode_deploy_ocp4
     elif [ "A${result}" == "AExit" ]
     then
+        PID=$(ps -A -o pid,cmd|grep qubinode-firstboot.sh | grep -v grep |head -n 1 | awk '{prin  t $1}')
+        if [ "A${PID}" != "A" ]
+        then
+             (kill -13 $PID 2>/dev/null)
+        fi
         exit 0
     else
         print "%s\n" " ${red}Unknown issue, please run the installer again${end}"
