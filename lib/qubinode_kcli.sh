79ac25750d2b70d66f268326df07634479b57142
Tosin Akinosho <takinosh@redhat.com>
2022-09-13 11:17:28 -0400

    rhel 8 backports

--- lib/qubinode_kcli.sh
+++ lib/qubinode_kcli.sh
@@ -7,6 +7,8 @@ source "${project_dir}/lib/qubinode_utils.sh"
   RHEL_VERSION=$(get_rhel_version)
   if [[ $RHEL_VERSION == "FEDORA" ]]; then
     defaults_file="/usr/lib/python$(python3 --version | grep -oe 3.10)/site-packages/kvirt/defaults.py"
+  elif [[ $RHEL_VERSION == "RHEL8" ]]; then
+    defaults_file="/usr/lib/python$(python3 --version | grep -oe 3.10)/site-packages/kvirt/defaults.py"
   elif [[ $(get_distro) == "centos" ]]; then
     defaults_file="/usr/lib/python$(python3 --version | grep -oe 3.9)/site-packages/kvirt/defaults.py"
   else 
