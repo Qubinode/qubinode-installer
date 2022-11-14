c67a679d92dd01e7f36be3561613c07dd4b64a5b
Tosin Akinosho <takinosh@redhat.com>
2022-11-13 23:46:47 -0500

    updating disconnected repo

--- lib/qubinode_baremetal_ipi_lab.sh
+++ lib/qubinode_baremetal_ipi_lab.sh
@@ -11,7 +11,7 @@ function qubinode_ipi_lab_maintenance () {
 	        configure_latest_ocp
             ;;
        configure_disconnected_repo)
-	        update_default_settings
+	        configure_disconnected_repo
             ;;
        install_packages)
 	        install_packages
@@ -26,6 +26,26 @@ function install_packages(){
   sudo dnf -y install podman httpd httpd-tools
 }
 function configure_disconnected_repo(){
+  sudo openssl req -newkey rsa:4096 -nodes -sha256 \
+    -keyout /nfs/registry/certs/domain.key -x509 -days 365 -out /nfs/registry/certs/domain.crt \
+    -subj "/C=US/ST=NorthCarolina/L=Raleigh/O=Red Hat/OU=Marketing/CN=provision.$GUID.dynamic.opentlc.com"
+
+  sudo cp /nfs/registry/certs/domain.crt $HOME/scripts/domain.crt
+  sudo chown lab-user $HOME/scripts/domain.crt
+  sudo cp /nfs/registry/certs/domain.crt /etc/pki/ca-trust/source/anchors/
+  sudo update-ca-trust extract
+  sudo htpasswd -bBc /nfs/registry/auth/htpasswd dummy dummy
+  sudo podman create --name poc-registry --net host -p 5000:5000 \
+      -v /nfs/registry/data:/var/lib/registry:z -v /nfs/registry/auth:/auth:z \
+      -e "REGISTRY_AUTH=htpasswd" -e "REGISTRY_AUTH_HTPASSWD_REALM=Registry" \
+      -e "REGISTRY_HTTP_SECRET=ALongRandomSecretForRegistry" \
+      -e REGISTRY_AUTH_HTPASSWD_PATH=/auth/htpasswd -v /nfs/registry/certs:/certs:z \
+      -e REGISTRY_HTTP_TLS_CERTIFICATE=/certs/domain.crt \
+      -e REGISTRY_HTTP_TLS_KEY=/certs/domain.key docker.io/library/registry:2
+  sudo podman start poc-registry
+  sudo podman ps
+  curl -u dummy:dummy -k \
+      https://provision.$GUID.dynamic.opentlc.com:5000/v2/_catalog
   echo "Configure a Disconnected Registry and Red Hat Enterprise Linux CoreOS Cache"
   printf "%s\n" " ${red}Configure a Disconnected Registry and Red Hat Enterprise Linux CoreOS Cache${end}"
   printf "%s\n" "Link: https://github.com/RHFieldProductManagement/baremetal-ipi-lab/blob/master/03-configure-local-registry-cache.md"
