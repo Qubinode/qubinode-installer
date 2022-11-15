274551916e31017ea765453260cd2cfb6174afe0
Tosin Akinosho <takinosh@redhat.com>
2022-11-15 12:40:15 -0500

    adding disconnected script

--- lib/qubinode_baremetal_ipi_lab.sh
+++ lib/qubinode_baremetal_ipi_lab.sh
@@ -113,35 +113,52 @@ function mirror_registry(){
 }
 
 function download_ocp_images(){
-  INSTALL_COMMIT=$(openshift-baremetal-install version | grep commit | cut -d' ' -f4)
-  IMAGE_JSON=$(curl -s \
-	https://raw.githubusercontent.com/openshift/installer/${INSTALL_COMMIT}/data/data/rhcos.json)
-  echo $IMAGE_JSON | jq .baseURI
-  sleep 3s
-  echo $IMAGE_JSON | jq .images.qemu
-  sleep 3s
-  echo $IMAGE_JSON | jq .images.openstack
-  sleep 3s
-  URL_BASE=$(echo $IMAGE_JSON | jq -r .baseURI)
-  QEMU_IMAGE_NAME=$(echo $IMAGE_JSON | jq -r .images.qemu.path)
-  QEMU_IMAGE_SHA256=$(echo $IMAGE_JSON | jq -r .images.qemu.sha256)
-  QEMU_IMAGE_UNCOMPRESSED_SHA256=$(echo $IMAGE_JSON | jq -r '.images.qemu."uncompressed-sha256"')
-  OPENSTACK_IMAGE_NAME=$(echo $IMAGE_JSON | jq -r .images.openstack.path)
-  OPENSTACK_IMAGE_SHA256=$(echo $IMAGE_JSON | jq -r .images.openstack.sha256)
-  curl -L -o ${IRONIC_DATA_DIR}/html/images/${QEMU_IMAGE_NAME} \
-	${URL_BASE}/${QEMU_IMAGE_NAME}
-  curl -L -o ${IRONIC_DATA_DIR}/html/images/${OPENSTACK_IMAGE_NAME} \
-	${URL_BASE}/${OPENSTACK_IMAGE_NAME}
-  echo "$QEMU_IMAGE_SHA256 ${IRONIC_DATA_DIR}/html/images/${QEMU_IMAGE_NAME}" \
-	| sha256sum -c
-  echo "$OPENSTACK_IMAGE_SHA256 ${IRONIC_DATA_DIR}/html/images/${OPENSTACK_IMAGE_NAME}" \
-	| sha256sum -c
-  RHCOS_QEMU_IMAGE=${QEMU_IMAGE_NAME}?sha256=${QEMU_IMAGE_UNCOMPRESSED_SHA256}
-  RHCOS_OPENSTACK_IMAGE=${OPENSTACK_IMAGE_NAME}?sha256=${OPENSTACK_IMAGE_SHA256}
+  export IRONIC_DATA_DIR=/nfs/ocp/ironic
+  export IRONIC_IMAGES_DIR="${IRONIC_DATA_DIR}/html/images"
+	export RHCOS_VERSION=$(openshift-baremetal-install coreos print-stream-json | jq -r '.["architectures"]["x86_64"]["artifacts"]["metal"]["release"]')
+	export RHCOS_ISO_URI=$(openshift-baremetal-install coreos print-stream-json | jq -r '.["architectures"]["x86_64"]["artifacts"]["metal"]["formats"]["iso"]["disk"]["location"]')
+	export RHCOS_ROOT_FS=$(openshift-baremetal-install coreos print-stream-json | jq -r '.["architectures"]["x86_64"]["artifacts"]["metal"]["formats"]["pxe"]["rootfs"]["location"]')
+	export RHCOS_QEMU_URI=$(openshift-baremetal-install coreos print-stream-json | jq -r '.["architectures"]["x86_64"]["artifacts"]["qemu"]["formats"]["qcow2.gz"]["disk"]["location"]')
+	export RHCOS_QEMU_SHA_UNCOMPRESSED=$(openshift-baremetal-install coreos print-stream-json | jq -r '.["architectures"]["x86_64"]["artifacts"]["qemu"]["formats"]["qcow2.gz"]["disk"]["uncompressed-sha256"]')
+	export RHCOS_OPENSTACK_URI=$(openshift-baremetal-install coreos print-stream-json | jq -r '.["architectures"]["x86_64"]["artifacts"]["openstack"]["formats"]["qcow2.gz"]["disk"]["location"]')
+	export RHCOS_OPENSTACK_SHA_COMPRESSED=$(openshift-baremetal-install coreos print-stream-json | jq -r '.["architectures"]["x86_64"]["artifacts"]["openstack"]["formats"]["qcow2.gz"]["disk"]["sha256"]')
+  export OCP_RELEASE=$(oc version | awk '{print $3}' | head -1)-x86_64
+  export OCP_RELEASE_DOWN_PATH=/var/www/html/$OCP_RELEASE
+
+  echo "RHCOS_VERSION: $RHCOS_VERSION"
+	echo "RHCOS_OPENSTACK_URI: $RHCOS_OPENSTACK_URI"
+	echo "RHCOS_OPENSTACK_SHA_COMPRESSED: ${RHCOS_OPENSTACK_SHA_COMPRESSED}"
+	echo "RHCOS_QEMU_URI: $RHCOS_QEMU_URI"
+	echo "RHCOS_QEMU_SHA_UNCOMPRESSED: $RHCOS_QEMU_SHA_UNCOMPRESSED"
+	echo "RHCOS_ISO_URI: $RHCOS_ISO_URI"
+	echo "RHCOS_ROOT_FS: $RHCOS_ROOT_FS"
+	echo "Press Enter to continue or Ctrl-C to cancel download"
+	read
+
+	if [[ ! -d ${OCP_RELEASE_DOWN_PATH} ]]; then
+		echo "----> Downloading RHCOS resources to ${OCP_RELEASE_DOWN_PATH}"
+		sudo mkdir -p ${OCP_RELEASE_DOWN_PATH}
+		echo "--> Downloading RHCOS resources: RHCOS QEMU Image"
+		sudo curl -s -L -o ${OCP_RELEASE_DOWN_PATH}/$(echo $RHCOS_QEMU_URI | xargs basename) ${RHCOS_QEMU_URI}
+		echo "--> Downloading RHCOS resources: RHCOS Openstack Image"
+		sudo curl -s -L -o ${OCP_RELEASE_DOWN_PATH}/$(echo $RHCOS_OPENSTACK_URI | xargs basename) ${RHCOS_OPENSTACK_URI}
+		echo "--> Downloading RHCOS resources: RHCOS ISO"
+		sudo curl -s -L -o ${OCP_RELEASE_DOWN_PATH}/$(echo $RHCOS_ISO_URI | xargs basename) ${RHCOS_ISO_URI}
+		echo "--> Downloading RHCOS resources: RHCOS RootFS"
+		sudo curl -s -L -o ${OCP_RELEASE_DOWN_PATH}/$(echo $RHCOS_ROOT_FS | xargs basename) ${RHCOS_ROOT_FS}
+	else
+		echo "The folder already exist, so delete it if you want to re-download the RHCOS resources"
+	fi
+
+  RHCOS_QEMU_IMAGE=${RHCOS_QEMU_URI}?sha256=${RHCOS_QEMU_SHA_UNCOMPRESSED}
+  RHCOS_OPENSTACK_IMAGE=${RHCOS_OPENSTACK_URI}?sha256=${RHCOS_OPENSTACK_SHA_COMPRESSED}
   sed -i "s/RHCOS_QEMU_IMAGE/$RHCOS_QEMU_IMAGE/g" \
 	$HOME/scripts/install-config.yaml
   sed -i "s/RHCOS_OPENSTACK_IMAGE/$RHCOS_OPENSTACK_IMAGE/g" \
 	$HOME/scripts/install-config.yaml
+  echo "Review Output of install-config.yaml"
+  read
+  cat install-config.yaml | less
 }
 
 function configure_latest_ocp_client(){
