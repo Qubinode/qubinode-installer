d0ab2b29b67a6623bfdefb9a6d30e4e44911f25b
Tosin Akinosho <takinosh@redhat.com>
2022-12-19 16:39:12 -0500

    adding scripts for ceph

---
+++ kcli_plans/ceph/ceph-mon01.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo subscription-manager refresh
+sudo subscription-manager attach --auto
+subscription-manager repos --disable=*
+subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rpms
+subscription-manager repos --enable=rhel-8-for-x86_64-appstream-rpms
+dnf update -y
+subscription-manager repos --enable=rhceph-5-tools-for-rhel-8-x86_64-rpmss
+subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms
+dnf install ansible cephadm-ansible -y
+sleep 120s
+nmcli con mod "System eth0" ipv4.dns "192.168.1.39"
+sudo nmcli con mod "System eth0" ipv4.dns-search "lab.tosins-supermicro.io"
+sudo service NetworkManager restart
---
+++ kcli_plans/ceph/ceph-nodes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo subscription-manager refresh
+sudo subscription-manager attach --auto
+subscription-manager repos --disable=*
+subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rpms
+subscription-manager repos --enable=rhel-8-for-x86_64-appstream-rpms
+dnf update -y
+subscription-manager repos --enable=rhceph-5-tools-for-rhel-8-x86_64-rpms
+sleep 60s
+nmcli con mod "System eth0" ipv4.dns "192.168.1.39"
+sudo nmcli con mod "System eth0" ipv4.dns-search "lab.tosins-supermicro.io"
+sudo service NetworkManager restart
