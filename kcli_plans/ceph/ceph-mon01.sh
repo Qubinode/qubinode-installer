0973a59913b14cc5697de9ecdc1ca1e702a6180e
Tosin Akinosho <takinosh@redhat.com>
2022-12-19 17:40:34 -0500

    udpating code

--- kcli_plans/ceph/ceph-mon01.sh
+++ kcli_plans/ceph/ceph-mon01.sh
@@ -5,10 +5,10 @@ subscription-manager repos --disable=*
 subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rpms
 subscription-manager repos --enable=rhel-8-for-x86_64-appstream-rpms
 dnf update -y
-subscription-manager repos --enable=rhceph-5-tools-for-rhel-8-x86_64-rpmss
+subscription-manager repos --enable=rhceph-5-tools-for-rhel-8-x86_64-rpms
 subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms
 dnf install ansible cephadm-ansible -y
-sleep 120s
+sleep 30s
 nmcli con mod "System eth0" ipv4.dns "192.168.1.39"
 sudo nmcli con mod "System eth0" ipv4.dns-search "lab.tosins-supermicro.io"
 sudo service NetworkManager restart
--- kcli_plans/ceph/ceph-nodes.sh
+++ kcli_plans/ceph/ceph-nodes.sh
@@ -6,7 +6,7 @@ subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rpms
 subscription-manager repos --enable=rhel-8-for-x86_64-appstream-rpms
 dnf update -y
 subscription-manager repos --enable=rhceph-5-tools-for-rhel-8-x86_64-rpms
-sleep 60s
+sleep 30s
 nmcli con mod "System eth0" ipv4.dns "192.168.1.39"
 sudo nmcli con mod "System eth0" ipv4.dns-search "lab.tosins-supermicro.io"
 sudo service NetworkManager restart
