89a846b1675eb600b9113ddac3d3fc6f9d5fccbb
Tosin Akinosho <takinosh@redhat.com>
2022-12-21 13:07:35 -0500

    udpating script for ceph deployments

--- kcli_plans/ceph/ceph-mon01.sh
+++ kcli_plans/ceph/ceph-mon01.sh
@@ -15,7 +15,7 @@ subscription-manager repos --enable=rhel-8-for-x86_64-appstream-rpms
 dnf update -y
 subscription-manager repos --enable=rhceph-5-tools-for-rhel-8-x86_64-rpms
 subscription-manager repos --enable=ansible-2.9-for-rhel-8-x86_64-rpms
-dnf install ansible cephadm-ansible -y
+dnf install ansible cephadm-ansible bind-utils -y
 sleep 30s
 nmcli con mod "System eth0" ipv4.dns "192.168.1.39"
 sudo nmcli con mod "System eth0" ipv4.dns-search "lab.tosins-supermicro.io"
--- kcli_plans/ceph/rhel8_ceph.sh
+++ kcli_plans/ceph/rhel8_ceph.sh
@@ -7,10 +7,10 @@ cd /usr/share/cephadm-ansible
 
 for i in {1..3}
 do
-    echo ceph-mon0${i}
-    ssh-copy-id root@ceph-mon0${i}
-    echo ceph-osd0${i}
-    ssh-copy-id root@ceph-osd0${i}
+    echo ceph-mon0${i}.$(hostname -d)
+    ssh-copy-id root@ceph-mon0${i}.$(hostname -d)
+    echo ceph-osd0${i}.$(hostname -d)
+    ssh-copy-id root@ceph-osd0${i}.$(hostname -d)
 done
 
 
@@ -27,42 +27,13 @@ EOF
 
 ansible-playbook -i hosts cephadm-preflight.yml 
 
-cat >bootstrap.yml<<EOF
----
-- name: bootstrap the cluster
-  hosts: ceph-mon01
-  become: true
-  gather_facts: false
-  tasks:
-    - name: login to registry
-      cephadm_registry_login:
-        state: login
-        docker: false
-        registry_url: registry.redhat.io
-        registry_username: RHEL_USERNAME
-        registry_password: RHEL_PASSWORD
-
-    - name: bootstrap initial cluster
-      cephadm_bootstrap:
-        mon_ip: "{{ monitor_address }}"
-        dashboard_user: admin
-        dashboard_password: yourgoingtohavetochangeme
-        allow_fqdn_hostname: true
-        cluster_network: 10.10.128.0/28
-
-EOF
-
-
-
-
-#ansible-playbook -i hosts bootstrap.yml -vvv --extra-vars "ceph_origin=rhcs"
 
 ansible-galaxy collection install containers.podman
 
 cat >bootstrap-nodes.yml<<EOF
 ---
 - name: bootstrap the nodes
-  hosts: all,!ceph-mon01
+  hosts: all
   become: true
   gather_facts: false
   tasks:
@@ -80,73 +51,43 @@ cat >bootstrap-nodes.yml<<EOF
         registry: registry.redhat.io
 EOF
 
+
 ansible-playbook -i hosts bootstrap-nodes.yml -vvv
 cephadm bootstrap --mon-ip $(hostname -I) --allow-fqdn-hostname
 cephadm shell ceph -s
 ceph -s
 
-#ceph orch host add ceph-mon02.example.com 192.168.56.65
-#ceph orch host add ceph-mon03.example.com 192.168.56.65
-
-cat >add-hosts.yml<<EOF
----
-- name: add additional hosts to the cluster
-  hosts: all
-  become: true
-  gather_facts: true
-  tasks:
-    - name: add hosts to the cluster
-      ceph_orch_host:
-        name: "{{ ansible_facts['hostname'] }}"
-        address: "{{ ansible_facts['default_ipv4']['address'] }}"
-        labels: "{{ labels }}"
-      delegate_to: ceph-mon01
-
-    - name: list hosts in the cluster
-      when: inventory_hostname in groups['admin']
-      ansible.builtin.shell:
-        cmd: ceph orch host ls
-      register: host_list
-
-    - name: print current list of hosts
-      when: inventory_hostname in groups['admin']
-      debug:
-        msg: "{{ host_list.stdout }}"
-EOF
-
-#ansible-playbook -i hosts cephadm-distribute-ssh-key.yml -e cephadm_ssh_user=root -e admin_node=ceph-mon01
-
-#ansible-playbook -i hosts add-hosts.yml
-
-cat >deploy_osd_service.yml<<EOF
----
-- name: deploy osd service
-  hosts: ceph-mon01
-  become: true
-  gather_facts: true
-  tasks:
-    - name: apply osd spec
-      ceph_orch_apply:
-        spec: |
-          service_type: osd
-          service_id: osd
-          placement:
-            host_pattern: '*'
-            label: osd
-          spec:
-            data_devices:
-              all: true
-EOF
-
-#ansible-playbook -i hosts deploy_osd_service.yml
-
-#ceph -s
-#ceph health
-#ceph osd tree
-
-echo "Ceph to consume any available and unused storage device:"
-# https://docs.ceph.com/en/latest/cephadm/services/osd/
-# ceph osd pool delete {pool-name} [{pool-name} --yes-i-really-really-mean-it
+ceph cephadm get-pub-key > ~/ceph.pub
+ssh-copy-id -f -i ~/ceph.pub root@$(dig ceph-mon02.$(hostname -d) +short)
+ssh-copy-id -f -i ~/ceph.pub root@$(dig ceph-mon03.$(hostname -d) +short)
+ssh-copy-id -f -i ~/ceph.pub root@$(dig ceph-osd01.$(hostname -d) +short)
+ssh-copy-id -f -i ~/ceph.pub root@$(dig ceph-osd02.$(hostname -d) +short)
+ssh-copy-id -f -i ~/ceph.pub root@$(dig ceph-osd03.$(hostname -d) +short)
+ceph orch host add ceph-mon02 $(dig ceph-mon02.$(hostname -d) +short)
+ceph orch host add ceph-mon03 $(dig ceph-mon03.$(hostname -d) +short)
+ceph orch host label add ceph-mon01 mon
+ceph orch host label add ceph-mon02 mon
+ceph orch host label add ceph-mon03 mon
+ceph orch apply mon ceph-mon01,ceph-mon02,ceph-mon03
+ceph orch host ls
+ceph orch ps
+sleep 120s
+ceph orch host add ceph-osd01 $(dig ceph-osd01.$(hostname -d) +short)
+ceph orch host add ceph-osd02 $(dig ceph-osd02.$(hostname -d) +short)
+ceph orch host add ceph-osd03 $(dig ceph-osd03.$(hostname -d) +short)
+ceph orch host label add ceph-osd01 osd
+ceph orch host label add ceph-osd02 osd
+ceph orch host label add ceph-osd03 osd
+sleep 120s
+ceph orch apply osd --all-available-devices
 #ceph orch daemon add osd ceph-osd01:/dev/vdb
 #ceph orch daemon add osd ceph-osd02:/dev/vdb
 #ceph orch daemon add osd ceph-osd03:/dev/vdb
+sleep 120s 
+ceph osd tree
+ceph osd pool create ocs 64 64
+#ceph osd pool application enable ocs rbd
+
+
+
+
