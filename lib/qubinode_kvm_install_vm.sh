4f0f9bc6c215542ca148fea41b3e2f8ce2f89af3
Tosin Akinosho <takinosh@redhat.com>
2022-12-26 13:29:04 -0500

    updating code

--- lib/qubinode_kvm_install_vm.sh
+++ lib/qubinode_kvm_install_vm.sh
@@ -2,4 +2,10 @@ setup_variables
 product_in_use=kvm-install-vm
 source "${project_dir}/lib/qubinode_utils.sh"
 
-echo "Configure kvm-install-vm"
+function qubinode_setup_kvm_install_vm(){
+    echo "Configure kvm-install-vm"
+}
+
+function qubinode_kvm_install_vm_maintenance(){
+    echo "function qubinode_kvm_install_vm_maintenance"
+}
