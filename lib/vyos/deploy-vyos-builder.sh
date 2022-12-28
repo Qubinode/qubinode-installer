63eccb94368d2913bef6ed64293cf85b403c6b12
Tosin Akinosho <takinosh@redhat.com>
2022-12-28 15:55:22 -0500

    updating code

--- lib/vyos/deploy-vyos-builder.sh
+++ lib/vyos/deploy-vyos-builder.sh
@@ -14,12 +14,13 @@ function destory_vm(){
 
 function deploy_vyos_builder_vm(){
     sudo  kvm-install-vm  create -t  debian10  -c 2 -m 4096 -d 60  -l /var/lib/libvirt/images/ -L /var/lib/libvirt/images/ vyos-builder
+    sleep 60s
     echo "Run the following commands"
     echo "*************************"
     echo "sudo su - root "
     IPADDR=$(sudo virsh net-dhcp-leases default | grep vyos-builder | awk '{print $5}' | sed 's/\/24//g')
     echo 'ssh -i /root/.ssh/id_rsa  debian@'${IPADDR}''
-    sudo virsh net-dhcp-leases default
+    #sudo virsh net-dhcp-leases default
 }
 
 
