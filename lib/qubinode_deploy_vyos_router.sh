f7dabc8e777a6ec55206893abee745c4d0f72607
Tosin Akinosho <takinosh@redhat.com>
2023-01-03 12:12:28 -0500

    testing

--- lib/qubinode_deploy_vyos_router.sh
+++ lib/qubinode_deploy_vyos_router.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
+    echo "$3 $4 $5"
 function vyos_variables () {
     setup_variables
     vars_file="${project_dir}/playbooks/vars/all.yml"
     kvm_host_vars_file="${project_dir}/playbooks/vars/kvm_host.yml"
     SUBNET=$(cat "${kvm_host_vars_file}" | grep kvm_subnet: | awk '{print $2}')
     USE_BRIDGE=$(cat "${kvm_host_vars_file}" | grep use_vyos_bridge: | awk '{print $2}')
-    echo "$3 $4 $5"
 }
 
 
