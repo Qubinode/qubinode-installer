79abca8355dd9cf82e065c41aaf281c669cc9aad
Tosin Akinosho <takinosh@redhat.com>
2023-01-10 19:21:21 -0500

    adding ocp4-ai

--- lib/qubinode_ai_sno.sh
+++ lib/qubinode_ai_sno.sh
@@ -8,9 +8,32 @@ function ai_sno_variables () {
 }
 
 
+function check_dependencies(){
+  echo "checking dependencies"
+  if [ ! -f $HOME/ocp-pull-secret ];
+  then 
+    echo "Please download the pull secret from hhttps://cloud.redhat.com/openshift/install/pull-secret"
+    echo "and save it to $HOME/ocp-pull-secret"
+    exit 1
+  fi
+
+  if [ ! -f $HOME/ssh_key.pub ];
+  then 
+    echo "Please create a ssh key and save it to $HOME/ssh_key.pub"
+    exit 1
+  fi
+
+  if [ ! -f $HOME/rh-api-offline-token ];
+  then 
+    echo "Please download the offline token from https://access.redhat.com/management/api"
+    echo "and save it to $HOME/rh-api-offline-token"
+    exit 1
+  fi
+}
 function create(){
   echo "creating SNO  deoployment using assisted installer"
   echo "https://docs.openshift.com/container-platform/4.9/installing/installing_sno/install-sno-installing-sno.html"
+  check_dependencies
 }
 
 function destroy(){
